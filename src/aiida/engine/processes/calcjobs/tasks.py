76d9d8a0aff6d4f2d0e0c61fa89c1b7093797e6a
Riccardo Bertossa <rbertoss@materys.com>
2025-10-14 14:47:38 +0200

    exponential_backoff accepts a max_interval
    this is useful if we want to provide an "illimitate" backoff (for
    example in the case we are waiting a user interaction to authenticate on
    the remote HPC) and we don't want to _manually_ play all paused
    workflow.
    
    useful for example in a cloud system, where implementing the event
    reaction system can be much more complex

--- src/aiida/engine/processes/calcjobs/tasks.py
+++ src/aiida/engine/processes/calcjobs/tasks.py
@@ -47,6 +47,7 @@ STASH_COMMAND = 'stash'
 KILL_COMMAND = 'kill'
 
 RETRY_INTERVAL_OPTION = 'transport.task_retry_initial_interval'
+MAX_INTERVAL_OPTION = 'transport.task_retry_max_interval'
 MAX_ATTEMPTS_OPTION = 'transport.task_maximum_attempts'
 
 logger = logging.getLogger(__name__)
@@ -78,6 +79,8 @@ async def task_upload_job(process: 'CalcJob', transport_queue: TransportQueue, c
 
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
+
     filepath_sandbox = get_config_option('storage.sandbox') or None
 
     authinfo = node.get_authinfo()
@@ -104,7 +107,8 @@ async def task_upload_job(process: 'CalcJob', transport_queue: TransportQueue, c
         logger.info(f'scheduled request to upload CalcJob<{node.pk}>')
         ignore_exceptions = (plumpy.futures.CancelledError, PreSubmitException, plumpy.process_states.Interruption)
         skip_submit = await utils.exponential_backoff_retry(
-            do_upload, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions
+            do_upload, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions,
+            max_interval=max_interval
         )
     except PreSubmitException:
         raise
@@ -140,6 +144,7 @@ async def task_submit_job(node: CalcJobNode, transport_queue: TransportQueue, ca
 
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
 
     authinfo = node.get_authinfo()
 
@@ -152,7 +157,8 @@ async def task_submit_job(node: CalcJobNode, transport_queue: TransportQueue, ca
         logger.info(f'scheduled request to submit CalcJob<{node.pk}>')
         ignore_exceptions = (plumpy.futures.CancelledError, plumpy.process_states.Interruption)
         result = await utils.exponential_backoff_retry(
-            do_submit, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions
+            do_submit, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions,
+            max_interval=max_interval
         )
     except (plumpy.futures.CancelledError, plumpy.process_states.Interruption):
         raise
@@ -186,6 +192,7 @@ async def task_update_job(node: CalcJobNode, job_manager, cancellable: Interrupt
 
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
 
     authinfo = node.get_authinfo()
     job_id = node.get_job_id()
@@ -219,7 +226,8 @@ async def task_update_job(node: CalcJobNode, job_manager, cancellable: Interrupt
             await asyncio.sleep(5)
 
         job_done = await utils.exponential_backoff_retry(
-            do_update, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions
+            do_update, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions,
+            max_interval=max_interval
         )
     except (plumpy.futures.CancelledError, plumpy.process_states.Interruption):
         raise
@@ -258,6 +266,7 @@ async def task_monitor_job(
 
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
     authinfo = node.get_authinfo()
 
     async def do_monitor():
@@ -269,7 +278,8 @@ async def task_monitor_job(
         logger.info(f'scheduled request to monitor CalcJob<{node.pk}>')
         ignore_exceptions = (plumpy.futures.CancelledError, plumpy.process_states.Interruption)
         monitor_result = await utils.exponential_backoff_retry(
-            do_monitor, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions
+            do_monitor, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions,
+            max_interval=max_interval
         )
     except (plumpy.futures.CancelledError, plumpy.process_states.Interruption):
         raise
@@ -304,6 +314,7 @@ async def task_retrieve_job(
         return
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
     authinfo = node.get_authinfo()
 
     async def do_retrieve():
@@ -337,7 +348,8 @@ async def task_retrieve_job(
         logger.info(f'scheduled request to retrieve CalcJob<{node.pk}>')
         ignore_exceptions = (plumpy.futures.CancelledError, plumpy.process_states.Interruption)
         result = await utils.exponential_backoff_retry(
-            do_retrieve, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions
+            do_retrieve, initial_interval, max_attempts, logger=node.logger, ignore_exceptions=ignore_exceptions,
+            max_interval=max_interval
         )
     except (plumpy.futures.CancelledError, plumpy.process_states.Interruption):
         raise
@@ -370,6 +382,7 @@ async def task_stash_job(node: CalcJobNode, transport_queue: TransportQueue, can
 
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
 
     authinfo = node.get_authinfo()
 
@@ -387,6 +400,7 @@ async def task_stash_job(node: CalcJobNode, transport_queue: TransportQueue, can
             max_attempts,
             logger=node.logger,
             ignore_exceptions=plumpy.process_states.Interruption,
+            max_interval=max_interval,
         )
     except plumpy.process_states.Interruption:
         raise
@@ -406,6 +420,7 @@ async def task_unstash_job(node: CalcJobNode, transport_queue: TransportQueue, c
 
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
 
     authinfo = node.get_authinfo()
 
@@ -423,6 +438,7 @@ async def task_unstash_job(node: CalcJobNode, transport_queue: TransportQueue, c
             max_attempts,
             logger=node.logger,
             ignore_exceptions=plumpy.process_states.Interruption,
+            max_interval=max_interval,
         )
     except plumpy.process_states.Interruption:
         raise
@@ -451,6 +467,7 @@ async def task_kill_job(node: CalcJobNode, transport_queue: TransportQueue, canc
     """
     initial_interval = get_config_option(RETRY_INTERVAL_OPTION)
     max_attempts = get_config_option(MAX_ATTEMPTS_OPTION)
+    max_interval = get_config_option(MAX_INTERVAL_OPTION)
 
     if node.get_state() in [CalcJobState.UPLOADING, CalcJobState.SUBMITTING]:
         logger.warning(f'CalcJob<{node.pk}> killed, it was in the {node.get_state()} state')
@@ -465,7 +482,8 @@ async def task_kill_job(node: CalcJobNode, transport_queue: TransportQueue, canc
 
     try:
         logger.info(f'scheduled request to kill CalcJob<{node.pk}>')
-        result = await utils.exponential_backoff_retry(do_kill, initial_interval, max_attempts, logger=node.logger)
+        result = await utils.exponential_backoff_retry(do_kill, initial_interval, max_attempts, logger=node.logger,
+                                                       max_interval=max_interval)
     except plumpy.process_states.Interruption:
         raise
     except Exception as exception:
